5273852153b0a415f7a01d5dea144e5b1872b8a1
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-15 17:40:34 +0530

    basic

---
+++ Basic/basicDemo1.js
@@ -0,0 +1,33 @@
+// console.log("Hello world !!")
+// console.info("Hello world !!!")
+// console.warn("Hello world !!!")
+// console.error("Hello world !!!")
+
+
+
+// Data type :- number , string , boolean , undefined , null
+a = 10;
+console.log(a);
+console.log(typeof(a)); 
+ 
+b = 12.67;
+
+console.log(b);
+console.log(typeof b);
+
+str = "Khushi"
+console.log(str);
+console.log(typeof str);
+
+isActive = true 
+console.log(isActive);
+console.log(typeof isActive);
+
+var c = 
+console.log(c);
+console.log(typeof c);
+
+
+x = "1234"
+console.log(x);
+console.log(typeof x);

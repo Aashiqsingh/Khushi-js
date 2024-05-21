9025d35b85c31187254eab72800e689ea90a8968
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-21 17:43:03 +0530

    Basic

---
+++ Basic/basicDemo2.js
@@ -0,0 +1,59 @@
+// console.log("Hello world!!")
+// console.info("Hello world!!")
+// console.warn("Hello world!!!")
+// console.error("Hello world!!!")
+
+// Data type :- number , string , boolean , undefined , null
+
+// a = 10;
+// console.log(a);
+// console.log(typeof a);
+
+// b = 12.67;
+
+// console.log(b);
+// console.log(typeof(b));
+
+// c = '67436'
+// console.log(c);
+// console.log(typeof(c));
+
+// isActive = true 
+// console.log(isActive);
+// console.log(typeof(isActive));
+
+
+// there are three keyword 
+// 1. var 
+// 2 . let 
+// 3 . const 
+
+var a = 10;
+console.log("outer a = ",a);
+
+let b = 20.2
+console.log("outer b = ",b);
+{
+    // console.log("inner a = ",a);
+    // console.log("inner b = ",b);
+
+    // var x = 25
+    // console.log("inner x = ",x);
+
+    // let y = 25.6
+    // console.log("inner y = ",y);
+
+    const pi = 3.14
+    console.log("pi = ",pi);
+}
+// console.log("outer x = ",x);
+// console.log("outer y = ",y);
+
+// console.log("Outer pi = ",pi);
+
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// console.log("outer i = ",i);
+
---
+++ Basic/basicDemo3.js
@@ -0,0 +1,21 @@
+// var x = 30
+
+// console.log(x);
+
+// var x = 50
+// console.log(x);
+
+
+// let x = 25
+// console.log(x);
+
+// x = 30
+// console.log(x);
+
+
+const pi = 3.14
+console.log(pi);
+
+p = 3.15
+
+console.log(pi);

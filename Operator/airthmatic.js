ea22787ac8cb8fccab9a00ac149cd5d7ee5bb907
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-28 17:57:55 +0530

    Operator

---
+++ Operator/airthmatic.js
@@ -0,0 +1,9 @@
+var a = 4
+var b = 3
+
+console.log("Addition = ",a+b);
+console.log("subtract = ",a-b);
+console.log("multiply = ",a*b);
+console.log("divide = ",a/b);  // quetiont
+console.log("module = ",a%b);   // remainder
+console.log("Exponent = ",a**b);

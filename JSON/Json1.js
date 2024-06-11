14761a47c66326c3d4f5d747c3e6d6e7f3f664a5
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-11 17:47:59 +0530

    json

---
+++ JSON/Json1.js
@@ -0,0 +1,20 @@
+const student ={
+    name: "Manu",
+    age: 25,
+    address: "Pune",
+    phone: 1234567890,
+    email: "manu@gmail.com"
+}
+
+console.log(student);
+
+// console.log(student.name);
+// console.log(student.address);
+// console.log(student.phone);
+
+// if(student.age > 18){
+//     console.log("You are allowed to vote");
+// }
+// else{
+//     console.log("You are not allowed to vote");
+// }

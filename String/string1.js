f2014e5c97c14bff6c6524c195065b51b55cb71e
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-11 17:27:08 +0530

    string

--- String/string1.js
+++ String/string1.js
@@ -26,7 +26,6 @@
 
 // x = 20
 // console.log("msg...",(x+10));
-
 // var str = "MerA Bharat mahaan"
 
 
@@ -50,10 +49,56 @@
 
 // @dhirajpoojara13
 
-var x = prompt("Enter the fullname")
+// var x = prompt("Enter the fullname")
 
-// var z = '@' + x + x.length
+// // var z = '@' + x + x.length
 
-var z = '@'.concat(x)
-var y = z.concat(x.length)
-console.log(y);
+// var z = '@'.concat(x)
+// var y = z.concat(x.length)
+// console.log(y);
+
+
+
+// var a = "royal technosoft";
+// console.log(a);
+
+
+
+
+// x.concat()
+
+
+
+var str = "      royal technosoft pvt ltd.       ";
+// console.log(str);
+// console.log(str.length);
+// var x = str.endsWith("td.")
+// var x = str.startsWith("royal t")
+// var x = str.replace("al" , "yl")
+// var x = str.replace("al" , "Royal")
+
+
+// var x = str.toUpperCase()
+// var x = str.toLowerCase()
+
+// var x = str.slice(6)
+// var x = str.slice(6 , 10)
+
+// var x = str.slice(-5 , -2)
+
+// var x = str.trim()
+
+var str2 = "Shree gurukrupa vidya sankul"
+console.log(str2);
+// var x = str2.substring(1 , 5)
+
+// var x = str2.split("u")
+
+
+
+
+console.log(x);
+// console.log(x.length);
+
+
+

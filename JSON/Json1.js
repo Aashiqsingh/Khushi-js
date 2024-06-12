00fa186b5a269cd6f0697e6662915087c4bced9b
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-12 17:26:08 +0530

    json

--- JSON/Json1.js
+++ JSON/Json1.js
@@ -1,12 +1,12 @@
-const student ={
-    name: "Manu",
-    age: 25,
-    address: "Pune",
-    phone: 1234567890,
-    email: "manu@gmail.com"
-}
+// const student ={
+//     name: "Manu",
+//     age: 25,
+//     address: "Pune",
+//     phone: 1234567890,
+//     email: "manu@gmail.com"
+// }
 
-console.log(student);
+// console.log(student);
 
 // console.log(student.name);
 // console.log(student.address);
@@ -18,3 +18,18 @@ console.log(student);
 // else{
 //     console.log("You are not allowed to vote");
 // }
+
+
+
+var employee = {
+    name:"Rahul",
+    age : 24,
+    salary : 220000,
+    Phone:768950373,
+    email:"rahul@gmail.com"
+}
+
+console.log(employee);
+
+console.log(employee.name);
+console.log(employee.age);
---
+++ JSON/json2.js
@@ -0,0 +1,38 @@
+// var Phone = {
+//     name : 'Iphone',
+//     price:563743,
+//     warranty: "2 years",
+//     IsAvailable : false,
+//     color:["red","green","black","white"],
+//     charge:"free delivery"
+// }
+
+// console.log(Phone);
+// console.log(Phone.name);
+
+
+// console.log(Phone.IsAvailable === true ? "available" : "not available");
+
+// console.log(Phone.color[0]);
+
+
+const employee = {
+    name:"Ayush",
+    age:23,
+    salary:50000,
+    phone:[6789767878 , 672894788898],
+    email:"ayush@gmail.com",
+    address:{
+        street: "C.G road",
+        Landmark:"Navrangpura",
+        city:"Ahmedabad",
+        State:"Gujarat",
+        pincode:380015,
+        country:"India"
+    }
+}
+
+console.log(employee);
+console.log(employee.address);
+console.log(employee.address.city);
+console.log(employee.phone[1]);

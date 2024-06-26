18f3ebc6521f7304b2c2e731f5d6b52bacd2e785
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-26 17:38:07 +0530

    Array

---
+++ Array/Array1.js
@@ -0,0 +1,37 @@
+// var arr = ["ram " , 34 , "ganesh", true]
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// console.log(arr.length);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+
+var user = ["ram","shyam","seeta","harsh","rahul","tathya","vrusti","jinendra"]
+
+console.log(user);
+
+// for(let i=0;i<user.length;i++){
+//     console.log(user[i]);
+// }
+
+// var x = user.push("khushi")
+// console.log(x);
+
+// var x = user.push("khushi","secret")
+// // console.log(x);
+// console.log(user);
+
+// var x = user.pop()
+// console.log(x);
+// console.log(user);
+
+
+// user.unshift("Aashiq")
+// console.log(user);
+
+// user.shift()
+// console.log(user);
---
+++ Array/Array2.js
@@ -0,0 +1,43 @@
+var user = ["ram","shyam","seeta","harsh","rahul","tathya","vrusti","jinendra"]
+
+
+// var x = user.splice(1 , 2)
+// console.log(x);
+
+// var x = user.splice(1,0,"Cow" ,"vrusti","jinendra")
+// console.log(x);
+// console.log(user);
+
+// var lang = ["hindi","english","marathi","gujarati"]
+
+// var x = user.concat(lang)
+// console.log(x);
+
+// var x = lang.concat(user)
+// console.log(x);
+
+
+// var x = lang.join("$")
+// console.log(x);
+
+// console.log(typeof x);
+
+// var x = lang.sort()
+// console.log(x);
+
+
+var num = [12,45,6,8,3,55,34]
+
+function sortbyValue(a,b){
+    return a-b;
+}
+
+
+// var x = num.sort(sortbyValue)
+// console.log(x);
+
+var x = user.indexOf("seeta")
+console.log(x);
+
+var x = user.reverse()
+console.log(x);

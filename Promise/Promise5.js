87dacdb64e5963737584c97eca74f4bc069664e7
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-18 17:54:42 +0530

    new promise

---
+++ Promise/Promise5.js
@@ -0,0 +1,53 @@
+const order = ()=>{
+
+    console.log("Order is about to be placed...");
+    
+    return promise = new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            resolve({
+                message:"Order is placed successfully..",
+                orderId: 1456,
+                amount:456
+            });
+            
+        },3000)
+    })
+
+    
+
+
+}
+
+const payment = (option)=>{
+
+    console.log("Amount is about to be payying...");
+
+    return promise = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve({
+                message:"Payment is successful..",
+                paymentId: option.orderId,
+                amount:option.amount
+            })
+        },4000)
+    })
+
+    
+ }
+    
+
+order().then((data)=>{
+    console.log("data...",data);
+    
+    payment(data).then((res)=>{
+        console.log("payement successful ...",res);
+        
+    }).catch((error)=>{
+        console.log("payment failed ...",error);
+        
+    })
+}).catch((err)=>{    
+    console.log("order failed...",err);
+    
+})

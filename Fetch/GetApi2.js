2757a0be5f1d64554b5dd2866e794e348f67f6cd
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-15 15:40:26 +0530

    deleteApi

--- Fetch/GetApi2.js
+++ Fetch/GetApi2.js
@@ -1,3 +1,5 @@
+
+
 const getData = async()=>{
 
     const res = await fetch("https://node5.onrender.com/user/user/",{
@@ -23,6 +25,13 @@ const getData = async()=>{
         var emailTd = document.createElement("td");
         var ageTd = document.createElement("td");
         var statusTd = document.createElement("td");
+        var btnTd = document.createElement("td");
+        var btn = document.createElement("button");
+        btn.innerHTML = "DELETE";
+
+        
+
+
 
         idTd.innerHTML = response.data[i]._id;
         nameTd.innerHTML = response.data[i].name;
@@ -30,15 +39,45 @@ const getData = async()=>{
         ageTd.innerHTML = response.data[i].age;
         statusTd.innerHTML = response.data[i].isActive;
 
+        btn.addEventListener("click",async()=>{
+
+            // alert("delete" + response.data[i]._id)
+
+            const response2 = await fetch("https://node5.onrender.com/user/user/"+response.data[i]._id,{
+                method:"DELETE",
+                
+            })
+
+            console.log(response2);
+            
+
+            if(response2.status === 204){
+                alert("User deleted successfully!!!")
+                data()
+            }
+            
+            
+
+        })
+
         tbody.appendChild(tr)
         tr.appendChild(idTd)
         tr.appendChild(nameTd)
         tr.appendChild(emailTd)
         tr.appendChild(ageTd)
         tr.appendChild(statusTd)
+        tr.appendChild(btnTd)
+        btnTd.appendChild(btn);
 
 
     }
     
+
     
 }
+
+function data(){
+    getData()  
+}
+
+getData()

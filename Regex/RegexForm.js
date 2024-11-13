84ccf825a2f41189d76a5155b7d3743b823c985c
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-13 14:42:26 +0530

    regex

---
+++ Regex/RegexForm.js
@@ -0,0 +1,55 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("submit",(event)=>{
+    event.preventDefault();
+
+    var name = document.getElementById("name")
+    var email = document.getElementById("email")
+    var mobile = document.getElementById("mobile")
+    var age = document.getElementById("age")
+
+    var nameErr = document.getElementById("nameErr")
+    var mobileErr = document.getElementById("mobileErr")
+    var emailErr = document.getElementById("emailErr")
+
+    var mobileRegex = /[6-9]{1}[0-9]{9}$/;
+    var userNameRegex = /[A-Za-z]+[0-9]+/;
+    const regex = /^[a-zA-Z0-9_]+@[a-z.]+\.[a-z]+$/;
+
+
+    if(email.value.match(regex)){
+        emailErr.innerHTML = ""
+    }
+    else{
+        emailErr.innerHTML = "Invalid email address"
+    }
+
+
+
+    // if(name.value.length < 3){
+    //     nameErr.innerHTML = "name must be at least 3 characters"
+    // }
+
+    // if(name.value.match(userNameRegex)){
+    //     nameErr.innerHTML = ""
+    // }
+    // else{
+    //     nameErr.innerHTML = "Invalid name"
+    // }
+
+
+    // if(mobile.value.match(mobileRegex)){
+    //     mobileErr.innerHTML = ""
+    // }
+    // else{
+    //     mobileErr.innerHTML = "Invalid mobile number"
+    // }
+
+
+
+
+    // console.log("name = ",name.value);
+    // console.log("email = ",email.value);
+    // console.log("mobile = ",mobile.value);
+    // console.log("age = ",age.value);
+    
+})
